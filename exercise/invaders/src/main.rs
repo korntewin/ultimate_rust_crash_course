f295a41e8bf19d070f440602c470b4220716d6e5
korntewin <korntewin.b@gmail.com>
2021-06-16 23:10:23 +0700

    add gameloop

--- exercise/invaders/src/main.rs
+++ exercise/invaders/src/main.rs
@@ -1,11 +1,41 @@
+use std::io;
 use std::error::Error;
+use std::time::Duration;
+use crossterm::event;
+use crossterm::{terminal, ExecutableCommand};
+use crossterm::cursor::{Hide,Show};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
-    audio.add("startup", "startup.wav");
-    audio.play("startup");
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
 
+    // terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(terminal::EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // game loop
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let event::Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    event::KeyCode::Esc | event::KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop
+                    },
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // Cleanup
+    stdout.execute(Show)?;
+    stdout.execute(terminal::LeaveAlternateScreen)?;
     audio.wait();
+
     Ok(())
 }
